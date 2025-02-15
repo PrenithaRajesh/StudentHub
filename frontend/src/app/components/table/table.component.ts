aac3f233aeea8d30466f2d1e51f7dfd878089cda
AnishJ3 <anish.joshi2021@vitstudent.ac.in>
2025-02-15 21:51:32 +0530

    angular: ag-grid setup

---
+++ frontend/src/app/components/table/table.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from '@angular/core';
+import { AgGridAngular } from 'ag-grid-angular';
+import { ButtonComponent } from '../button/button.component';
+
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.scss']
+})
+
+export class TableComponent implements OnInit {
+
+  defaultColDef = {
+
+  }
+
+  defaultColGroupDef = {
+  
+  }
+
+  frameworkComponents = [
+    
+  ]
+
+  columnDefs = [
+
+    // Radhika
+    {headerName:'ID', field: 'Id' , CellRenderer:'', cellEditor:''},
+
+    //Prenitha
+    { headerName:'Name',field: 'Name' , CellRenderer:'', cellEditor:''},
+
+    // Baibhav
+    { headerName:'Email',field: 'Email' , CellRenderer:'', cellEditor:''},
+
+    // Jyotsana
+    { headerName:'Address',field:'Address', CellRenderer:'', cellEditor:''},
+
+    // Paridhi and Anish
+    {headerName:'Marks' , field:'Marks', groupId:'MarksGroup', marryChildren:true,CellRenderer:'', cellEditor:'' ,
+
+      children:[
+        {headerName: 's1',columnGroupShow:'open'},
+        {headerName:'s2',columnGroupShow:'open'},
+        {headerName:'s3',columnGroupShow:'open'},
+        {headerName:'total' }
+      ]
+    }
+    
+  ];
+  rowData = []
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onGridReady(params:any)
+  {
+
+  }
+
+}

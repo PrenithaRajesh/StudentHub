080dc69ee3e703953d1ca0f17cbaa70eb80ef294
Paridhi502 <paridhi502@gmail.com>
2025-02-17 20:33:52 +0530

    angular: merged Paridhi and Baibhav's code

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,11 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddControllers();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy => policy.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+});
 
 var app = builder.Build();
 
@@ -15,9 +22,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-else{
-    app.UseHttpsRedirection();
-}
+
+app.UseCors("AllowAll");
+app.UseRouting();
+app.UseAuthorization(); 
 
 app.MapControllers();
 

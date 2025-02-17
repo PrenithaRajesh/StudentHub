d34fa2a66621d26a828c9cfe5253054303936b0b
Prenitha Rajesh <prenitharajesh04@gmail.com>
2025-02-17 22:54:27 +0530

    angular: update service path and API call
    Signed-off-by: Prenitha Rajesh <prenitharajesh04@gmail.com>

--- frontend/src/app/services/data.service.ts
+++ frontend/src/app/services/data.service.ts
@@ -4,7 +4,8 @@ import { Observable } from 'rxjs';
 @Injectable({ providedIn: 'root' })
 
 export class DataService{
-    private apiUrl = 'https://localhost:5001/api/User';
+    // DEV 
+    private apiUrl = 'http://localhost:5000/api/User';
     constructor(private http:HttpClient){}
     
     getUsers():Observable<any>{

d0cd5bf441762cf9a2510cf9901ddab08de89551
Jyotsna03 <jyotsnabh03@gmail.com>
2025-02-18 23:16:19 +0530

    Email Component Added-Jyotsna

--- frontend/src/app/components/email/email.component.ts
+++ frontend/src/app/components/email/email.component.ts
@@ -1,6 +1,6 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-import { EmailService } from '../services/email.service';
+import { EmailService } from '../../services/email.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({

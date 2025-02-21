7b360afef1f4a067af891eb22b4c35969c372a1c
Jyotsna Bhatia <69782687+Jyotsna03@users.noreply.github.com>
2025-02-21 20:00:53 +0530

    Update EmailModel.cs

--- backend/Models/EmailModel.cs
+++ backend/Models/EmailModel.cs
@@ -6,11 +6,11 @@ using System.Threading.Tasks;
 namespace backend.Models
 {
    public class EmailModel
-{
+   {
     public string SenderEmail { get; set; } = string.Empty;
     public string ReceiverEmail { get; set; } = string.Empty;
     public string Subject { get; set; } = string.Empty;
     public string Body { get; set; } = string.Empty;
     public List<IFormFile> Attachments { get; set; } = new List<IFormFile>();
-}
+   }
 }

797537516ea160cf4133b35544b498aadd095fe9
Paridhi502 <paridhi502@gmail.com>
2025-02-21 20:31:39 +0530

    angular: added delete functionality

--- frontend/src/app/services/email.service.ts
+++ frontend/src/app/services/email.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
+import { environment } from 'src/environment';
+import { Observable } from 'rxjs'
 import { Email } from '../models/email.model';
 
 @Injectable({
